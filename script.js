c8c9864d3a696b0ff5f6970e8972a2b8d869f935
hayLaz-53 <alisayyildiz0@gmail.com>
2024-10-20 12:16:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Pop-up'ı göster
+    
     document.getElementById("popup").style.display = "flex"; 
 });
 
 function closePopup() {
-    // Pop-up'ı kapat
+  
     document.getElementById("popup").style.display = "none"; 
 }
 
